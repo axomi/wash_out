740317f456d41a636c016597facdac3d17293f3e
Nic Barthelemy <nbarthel@axomi.com>
2017-12-11 16:33:00 -0800

    Disable use of HTTP_SOAPACTION header

--- lib/wash_out/router.rb
+++ lib/wash_out/router.rb
@@ -49,8 +49,9 @@ module WashOut
     def parse_soap_action(env)
       return env['wash_out.soap_action'] if env['wash_out.soap_action']
 
-      soap_action = controller.soap_config.soap_action_routing ? env['HTTP_SOAPACTION'].to_s.gsub(/^"(.*)"$/, '\1')
-                                                               : ''
+      #soap_action = controller.soap_config.soap_action_routing ? env['HTTP_SOAPACTION'].to_s.gsub(/^"(.*)"$/, '\1')
+      #                                                         : ''
+      soap_action = ""
       if soap_action.blank?
         parsed_soap_body = nori(controller.soap_config.snakecase_input).parse(soap_body env)
         return nil if parsed_soap_body.blank?
